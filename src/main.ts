368e587761cd79a455c2f69dd11639bb1442f0c6
Ritikjainkkar <ritikjainkkar@gmail.com>
2024-09-22 14:52:19 +0530

    change main.ts to previous version

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import * as express from 'express';
-
-const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
+  
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.init();
+
+  await app.listen(3000);
 }
-
 bootstrap();
-
-export default server;
