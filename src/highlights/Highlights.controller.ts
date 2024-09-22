f142817bdeebf2b0987200d375d0ee73b74940a8
Ritikjainkkar <ritikjainkkar@gmail.com>
2024-09-22 15:38:42 +0530

    fix

---
+++ src/highlights/Highlights.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { HighlightsService } from './Highlights.service';
+
+@Controller('highlights')
+export class HighlightsController {
+  constructor(private readonly highlightsService: HighlightsService) {}
+
+  @Get()
+  findAll() {
+    return this.highlightsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('text') text: string,
+    @Body('position') position: number,
+  ) {
+    return this.highlightsService.create(text, position);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body('text') text: string,
+    @Body('position') position: number,
+  ) {
+    return this.highlightsService.update(id, text, position);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    this.highlightsService.delete(id);
+    return { message: 'Item deleted successfully' };
+  }
+}
